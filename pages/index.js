84e9c5f76e84bfe4ee4a54a93e39aa0a96cf2b91
laurabulloch <s1928598@ed.ac.uk>
2023-06-14 16:37:44 +0100

    Add useEffect to call API

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 export default function Home() {
     const [enteredTown, setEnteredTown] = useState('');
 
+
   return (
     <div className={styles.container}>
 
--- pages/location.js
+++ pages/location.js
@@ -1,9 +1,24 @@
 import Link from "next/link";
 import { useSearchParams } from 'next/navigation'
+import {useEffect} from "react";
 
 export default function Location() {
     const searchParams = useSearchParams();
     const currentTown = searchParams.get('search');
+
+    useEffect(() => {
+        // eslint-disable-next-line no-undef
+        // axios.get(process.env.REACT_APP_API_URL + '/to-dos').then((response) => {
+        //     setToDos(response.data);
+        // });
+        try {
+            const res = fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=1e98395ba0fddf8081b39c2bbb20ecb1")
+                .then(response => response.json())
+                .then(data => console.log(data))
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
 
     return (
         <>
