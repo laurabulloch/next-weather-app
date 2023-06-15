e8e4a971a3c7ba6406103f849dc365ea29107114
laurabulloch <s1928598@ed.ac.uk>
2023-06-15 14:35:55 +0100

    get lat and longitude from API

--- pages/index.js
+++ pages/index.js
@@ -4,46 +4,67 @@ import {useState} from "react";
 import Link from "next/link";
 
 export default function Home() {
-    const [enteredTown, setEnteredTown] = useState('');
+    const [currentTown, setCurrentTown] = useState('');
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
 
+    async function getData() {
+        const response = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
+        if (!response.ok) {
+            // This will activate the closest `error.js` Error Boundary
+            throw new Error('Failed to fetch data')
+        }
+        return response.json();
 
-  return (
-    <div className={styles.container}>
+    }
 
-      <Head>
-        <title>Next Weather App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const getLongLat = async () => {
+        const fetchedData = await getData();
+        const longitude = fetchedData[0].lon;
+        const latitude = fetchedData[0].lat;
+        setLon(longitude);
+        setLat(latitude);
+    };
 
-      <main>
-          <h1 className={styles.title}>
-              Your Weather
-          </h1>
+    return (
+        <div className={styles.container}>
 
-          <p className={styles.description}>
-              Get started by typing a town into the search box below
-          </p>
+            <Head>
+                <title>Next Weather App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-          <div>
-              <input
-                  type="text"
-                  id="location"
-                  placeholder="Enter Town"
-                  value = {enteredTown}
-                  onChange = {(event) => setEnteredTown(event.target.value)}
-              />
-               <Link href={{
-                   pathname: "/location",
-                   query: {
-                       search: enteredTown
-                   }
-               }}
-               >
-                   <button id="btn">Go</button>
-               </Link>
-          </div>
+            <main>
+                <h1 className={styles.title}>
+                    Your Weather
+                </h1>
 
-      </main>
-      </div>
-  )
+                <p className={styles.description}>
+                    Get started by typing a town into the search box below
+                </p>
+
+                <div>
+                    <input
+                        type="text"
+                        id="location"
+                        placeholder="Enter Town"
+                        value={currentTown}
+                        onChange={(event) => setCurrentTown(event.target.value)}
+                    />
+                    <Link href={{
+                        pathname: "/location",
+                        query: {
+                            search: currentTown,
+                            longitude: lon,
+                            latitude: lat
+                        }
+                    }}
+                    >
+                        <button id="btn" onClick={getLongLat}>Go</button>
+                    </Link>
+                </div>
+
+            </main>
+        </div>
+    )
 }
--- pages/location.js
+++ pages/location.js
@@ -3,28 +3,32 @@ import { useSearchParams } from 'next/navigation'
 import {useEffect, useState} from "react";
 
 export default function Location() {
-    const [data, setData] = useState('');
+
     const searchParams = useSearchParams();
     const currentTown = searchParams.get('search');
+    const lon = searchParams.get('longitude');
+    const lat = searchParams.get('latitude');
 
-    useEffect(() => {
-        // eslint-disable-next-line no-undef
-        // axios.get(process.env.REACT_APP_API_URL + '/to-dos').then((response) => {
-        //     setToDos(response.data);
-        // });
-        console.log(process.env.REACT_APP_WEATHER_API_KEY);
-        try {
-            const res = fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=" + process.env.REACT_APP_WEATHER_API_KEY)
-                .then(response => response.json())
-                .then(data => setData(data))
-        } catch (err) {
-            console.log(err);
-        }
-    }, []);
+    // useEffect(() => {
+    //     try {
+    //         const res = fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
+    //             .then(response => response.json())
+    //             .then(data =>
+    //                 setData(data)
+    //             )
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    //     ;
+    //     console.log(data);
+    //
+    // }, []);
 
     return (
         <>
             <h1>{currentTown}</h1>
+            <h2>{lon}</h2>
+            <h2>{lat}</h2>
             <h2>
                 <Link href="/">Search again</Link>
             </h2>
