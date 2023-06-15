db5ee7cb052cc7a2317420b0e5a857cd927e6df0
laurabulloch <s1928598@ed.ac.uk>
2023-06-15 15:29:03 +0100

    displays lat and lon on location page

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,6 @@ import Link from "next/link";
 
 export default function Home() {
     const [currentTown, setCurrentTown] = useState('');
-    const [lat, setLat] = useState(0);
-    const [lon, setLon] = useState(0);
-
-    async function getData() {
-        const response = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
-        if (!response.ok) {
-            // This will activate the closest `error.js` Error Boundary
-            throw new Error('Failed to fetch data')
-        }
-        return response.json();
-
-    }
-
-    const getLongLat = async () => {
-        const fetchedData = await getData();
-        const longitude = fetchedData[0].lon;
-        const latitude = fetchedData[0].lat;
-        setLon(longitude);
-        setLat(latitude);
-    };
 
     return (
         <div className={styles.container}>
@@ -55,12 +35,10 @@ export default function Home() {
                         pathname: "/location",
                         query: {
                             search: currentTown,
-                            longitude: lon,
-                            latitude: lat
                         }
                     }}
                     >
-                        <button id="btn" onClick={getLongLat}>Go</button>
+                        <button id="btn" >Go</button>
                     </Link>
                 </div>
 
--- pages/location.js
+++ pages/location.js
@@ -6,8 +6,8 @@ export default function Location() {
 
     const searchParams = useSearchParams();
     const currentTown = searchParams.get('search');
-    const lon = searchParams.get('longitude');
-    const lat = searchParams.get('latitude');
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0)
 
     // useEffect(() => {
     //     try {
@@ -23,6 +23,55 @@ export default function Location() {
     //     console.log(data);
     //
     // }, []);
+
+    useEffect(() => {
+        async function fetchLocation() {
+            const response = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + currentTown + "&limit=1&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
+            if (!response.ok) {
+                // This will activate the closest `error.js` Error Boundary
+                throw new Error('Failed to fetch location data')
+            }
+            return response.json();
+
+        }
+
+        const getLongLat = async () => {
+            const fetchedData = await fetchLocation();
+            const longitude = fetchedData[0].lon;
+            const latitude = fetchedData[0].lat;
+            setLon(longitude);
+            setLat(latitude);
+        };
+
+        // async function fetchWeather(lat, lon) {
+        //     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
+        //     if (!response.ok) {
+        //         // This will activate the closest `error.js` Error Boundary
+        //         throw new Error('Failed to fetch weather data')
+        //     }
+        //     return response.json();
+        //
+        // }
+
+        // const fetchTemperature = async (lat,lon) => {
+        //     try {
+        //         //...
+        //         return responseData.sectors;
+        //     } catch (err) {}
+        // };
+        //
+        // const fetchData = async () => {
+        //     const tempLoadedContent = await fetchLocation();
+        //     const tempLoadedSectors = await fetchSectors(tempLoadedContent[0].id);
+        //     setLoadedContent(tempLoadedContent);
+        //     setLoadedSectors(tempLoadedSectors);
+        // };
+        //
+        //
+        // fetchData();
+        getLongLat();
+    }, []);
+
 
     return (
         <>
