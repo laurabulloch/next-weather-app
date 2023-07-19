ad170c5e17119403bf760dde1b93083d457a9f16
laurabulloch <s1928598@ed.ac.uk>
2023-07-19 12:17:18 +0100

    show sunride, sunset and image of current weather

--- pages/[location].js
+++ pages/[location].js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import styles from '../styles/Home.module.css';
 import Head from "next/head";
+import moment from "moment-timezone";
+import React from "react";
+import Image from "next/image";
 
 async function fetchWeather(lat, lon) {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + process.env.NEXT_PUBLIC_WEATHER_API_KEY)
@@ -36,6 +39,7 @@ export const getServerSideProps = async ({params}) => {
 
 export default function Location(props) {
     const temperature = props.locationWeather.main.temp - 273.15;
+    const timezone = props.locationWeather.timezone.toString();
 
     return (
         <div className={styles.container}>
@@ -50,11 +54,20 @@ export default function Location(props) {
             <h2>
                 <span>{(props.locationWeather.main.temp_max.toFixed(0))- 273}&deg;C</span>
                 <span>{(props.locationWeather.main.temp_min.toFixed(0))-273}&deg;C</span>
-            </h2>
 
-            <h2>
-                <Link href="/">Search another location</Link>
+                <span>{moment.unix(props.locationWeather.sys.sunrise).tz(timezone).format("LT")}</span>
+                <span>{moment.unix(props.locationWeather.sys.sunset).tz(timezone).format("LT")}</span>
             </h2>
+            <Image
+                src={`https://openweathermap.org/img/w/${props.locationWeather.weather[0].icon}.png`}
+                alt="Weather Icon"
+                width={150}
+                height={150}
+            />
+
+            <h3>
+                <Link href="/">Search another location</Link>
+            </h3>
         </div>
         )
 }
